53c685d32b7c3227a50d8ccae72c5081ac890495
MITHULVS <vsmithul2007@gmail.com>
2026-06-29 07:45:40 +0000

    test: redis deployment

--- movie_backend/util/helpers.py
+++ movie_backend/util/helpers.py
@@ -28,13 +28,11 @@ load_dotenv()
 
 REDIS_HOST = os.getenv("REDIS_HOST")
 REDIS_PORT = int(os.getenv("REDIS_PORT", 6379))
+REDIS_URL = os.getenv("REDIS_URL")
+print("REDIS_URL =", os.getenv("REDIS_URL"))
 
-print(f"REDIS_HOST = {REDIS_HOST}")
-print(f"REDIS_PORT = {REDIS_PORT}")
-
-redis_client = redis.Redis(
-    host=REDIS_HOST,
-    port=REDIS_PORT,
+redis_client = redis.from_url(
+    REDIS_URL,
     decode_responses=True,
 )
 
